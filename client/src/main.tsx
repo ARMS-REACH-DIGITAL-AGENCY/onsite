6ad955d79765fdb8294113c8b16db73faca2a616
YAT?STATS <pcdaction@gmail.com>
2026-07-16 09:52:06 -0700

    Enable OnSite branding and Pete enhancements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { installOnsiteEnhancements } from "./onsiteEnhancements";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+window.requestAnimationFrame(() => installOnsiteEnhancements());
