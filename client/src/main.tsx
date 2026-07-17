392377bb302e32acfa744a872e0d4c30c442fb8b
YAT?STATS <pcdaction@gmail.com>
2026-07-16 18:52:39 -0700

    Load final mobile compatibility stylesheet after all other styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,11 @@ import "./index.css";
 import "./productionPolish.css";
 import { installOnsiteEnhancements } from "./onsiteEnhancements";
 
+const mobileCss = document.createElement("link");
+mobileCss.rel = "stylesheet";
+mobileCss.href = "/mobileCompatibility.css?v=20260716-final";
+document.head.appendChild(mobileCss);
+
 createRoot(document.getElementById("root")!).render(<App />);
 
 window.requestAnimationFrame(() => installOnsiteEnhancements());
