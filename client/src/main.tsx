a466d5905ad2fd1869cb5317eef6a8d5627b2d44
YAT?STATS <pcdaction@gmail.com>
2026-07-16 18:31:15 -0700

    Load approved production polish for Pete card and mobile navigation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./productionPolish.css";
 import { installOnsiteEnhancements } from "./onsiteEnhancements";
 
 createRoot(document.getElementById("root")!).render(<App />);
